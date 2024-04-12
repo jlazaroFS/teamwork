acc47331d14560d167bc06f6c2bf253bd7441c8d
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-04-12 13:03:23 +0200

    allow crossorigin for getAllEmpleado

--- src/main/java/com/futurespace/teamwork/controller/EmpleadoController.java
+++ src/main/java/com/futurespace/teamwork/controller/EmpleadoController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class EmpleadoController {
         this.service = service;
     }
 
+    @CrossOrigin(origins = "http://localhost:8081")
     @GetMapping
     public ResponseEntity<List<Empleado>> getAllEmpleado() {
         List<Empleado> empleados = service.getAllEmpleado();
